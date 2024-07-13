d902a05637c466416ad9437a384b095238474aff
Jose Luis Sáez <joseluissaez@me.com>
2024-07-13 23:24:21 +0200

    Seguridad añadida

---
+++ src/main/java/com/jlsaez/hospitalclinicovetapi/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.jlsaez.hospitalclinicovetapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/h2-console/**").permitAll() // Permitir acceso a la consola H2
+                                .requestMatchers("/hospitalclinicovet-api/**").authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(csrf -> csrf.disable()) // Ignorar CSRF para que funcionen las pruebas put, delete y post con postman
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin())); // Permitir que la consola H2 se cargue en un frame
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("jlsaez")
+                .password(passwordEncoder.encode("seleccioname"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
